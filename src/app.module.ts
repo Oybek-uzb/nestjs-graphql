230ce44a4669a76fa5d6795c2804a4c83fdebc3b
Oybek-uzb <78894838+Oybek-uzb@users.noreply.github.com>
2022-05-08 15:42:51 +0500

    migrating to GraphQL+TypeScript v10
    connecting special derivers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from "@nestjs/graphql";
 import { UsersModel } from './users/users.module';
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
-      autoSchemaFile: true
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
     }),
-    UsersModel
+    UsersModel,
   ],
   controllers: [],
   providers: [],
